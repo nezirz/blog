a41b153cc46e5d3e664a14bdeeb7fbcd59ae8a29
zirexba <zirexba@hotmail.com>
2015-10-05 13:45:31 +0200

    Fix About and Contact page access

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,6 +1,8 @@
 class ContactController < ApplicationController
   def index
+  	if user_signed_in?
       @recentposts = Post.where("user_id==#{current_user.id}").last(5).reverse
       @recentcomments = Comment.where("user_id==#{current_user.id}").last(5).reverse  	
+   end
   end
 end
