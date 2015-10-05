5942128d1194b0d71ad88e95d3efe508003996a4
zirexba <zirexba@hotmail.com>
2015-10-05 13:26:12 +0200

    Fix Devise Registration redirect, add css on Sign In and Sign Up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
     if user_signed_in?
 
       @recentposts = Post.where("user_id==#{current_user.id}").last(5).reverse
-      #@recentcomments = Comment.where("user_id==#{current_user.id}").last(5).reverse
+      @recentcomments = Comment.where("user_id==#{current_user.id}").last(5).reverse
      
     else
 
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,14 +1,24 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
-  def after_sign_in_path_for(users)
-    flash[:notice] = 'Welcome! Please follow the steps!'
-    
-    redirect_to posts_path
+  def after_sign_in_path_for(posts)
+    #flash[:notice] = 'Welcome! Please follow the steps!'
+    case resource
+    when :user, User
+      resource.teacher? ? another_path : root_path
+    else
+      super
+    end
+    #redirect_to posts_path
   end
-  def after_sign_up_path_for(users)
-    flash[:notice] = 'Welcome! Please follow the steps!'
-    
-    redirect_to posts_path
+  def after_sign_up_path_for(posts)
+    #flash[:notice] = 'Welcome! Please follow the steps!'
+    case resource
+    when :user, User
+      root_path
+    else
+      super
+    end
+    #redirect_to posts_path
   end
 end
